66b9cc570020e5131da53522acb5c3379aedb9f7
ihollander <ianholla@gmail.com>
2020-02-14 07:11:33 -0500

    media query fix

---
+++ temp.js
@@ -0,0 +1,12 @@
+// 1.
+// given an array of names
+// create a button with each name as the text content
+// when the button is clicked
+// make a request to your backend and create a new ___ with the name from the button
+// after the fetch, use some info from the server to update the DOM 
+// (add a class of 'done', add a data-id, etc.)
+
+// 2.
+// when I click on a name
+// make a request to your server and update the name by reversing it
+// after the fetch, use the reversed name to update the DOM
